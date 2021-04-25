48de7737e7e55f64d290364e4442c38829c2e892
Kaushik-Indukuri <49111141+Kaushik-Indukuri@users.noreply.github.com>
2021-04-25 11:13:05 -0700

    final edits

--- trie/services/server.py
+++ trie/services/server.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from trie import Trie
 import json
 
+__author__ = "Kaushik"
 
 app = Flask(__name__)
 t = Trie()
@@ -12,20 +13,27 @@ def query():
     args = request.args
     if "word" in args:
         word = args.get("word")
+    if not word:
+        flask.abort(404)
     word = word.strip()
     global in_trie
     in_trie = t.search(word)
     return word
 
 
+@app.route('/')
+def home():
+    return '<h1>Trie Project</h1>'
+
+
 @app.route('/add', methods=["POST"])
 def add():
     word = query()
     if in_trie:
-        return json.dumps({"output": word + " is already in the trie"})
+        return flask.jsonify({"output": word + " is already in the trie"})
     elif not in_trie:
         t.add(word)
-        return json.dumps({"output": word + " has been added"})
+        return flask.jsonify({"output": word + " has been added"})
     else:
         raise ValueError("Error, try again")
 
@@ -35,9 +43,9 @@ def delete():
     word = query()
     if in_trie:
         t.delete(word)
-        return json.dumps({"output": word + " has been deleted"})
+        return flask.jsonify({"output": word + " has been deleted"})
     elif not in_trie:
-        return json.dumps({"output": word + " is not in the trie"})
+        return flask.jsonify({"output": word + " is not in the trie"})
     else:
         raise ValueError("Error, try again")
 
@@ -46,9 +54,9 @@ def delete():
 def search():
     word = query()
     if in_trie:
-        return json.dumps({"output": word + " was found in the trie"})
+        return flask.jsonify({"output": word + " was found in the trie"})
     elif not in_trie:
-        return json.dumps({"output": word + " was not found in the trie"})
+        return flask.jsonify({"output": word + " was not found in the trie"})
     else:
         raise ValueError("Error, try again")
 
@@ -58,9 +66,9 @@ def autocomplete():
     word = query()
     response = t.autocomplete(word)
     if response is None:
-        return json.dumps({"output": "No autocomplete suggestions"})
+        return flask.jsonify({"output": "No autocomplete suggestions"})
     elif response is not None:
-        return json.dumps({"output": "Autocomplete suggestions for " + word + ": {}".format(response)})
+        return flask.jsonify({"output": "Autocomplete suggestions for " + word + ": {}".format(response)})
     else:
         raise ValueError("Error, try again")
 
@@ -68,10 +76,10 @@ def autocomplete():
 @app.route('/display', methods=["GET"])
 def display():
     if len(t.display()) == 0:
-        return json.dumps({"output": "Trie is empty"})
+        return flask.jsonify({"output": "Trie is empty"})
     elif len(t.display()) >= 1:
         response = t.display()
-        return json.dumps({"output": "Trie: {}".format(response)})
+        return flask.jsonify({"output": "Trie: {}".format(response)})
     else:
         raise ValueError("Error, try again")
 
