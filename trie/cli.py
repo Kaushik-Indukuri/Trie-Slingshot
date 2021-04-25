2198cc9961b80699f92a3364e95988c1e25a2897
Kaushik-Indukuri <49111141+Kaushik-Indukuri@users.noreply.github.com>
2021-04-25 07:02:14 -0700

    update server

--- trie/cli.py
+++ trie/cli.py
@@ -10,7 +10,7 @@ class Context:
 
 @click.group()
 @click.option('-w', '--word', type=str, help='Input word')
-@click.option('-s', '--server', envvar='TRIE_SERVER', default='http://13.52.104.70:8080', help='Server')
+@click.option('-s', '--server', envvar='TRIE_SERVER', default='http://54.177.45.15:8080', help='Server')
 @click.pass_context
 def cli(ctx, word, server):
     """Welcome to My Trie System"""
@@ -21,44 +21,49 @@ def cli(ctx, word, server):
 @click.pass_context
 def add(ctx):
     """Add word"""
-    response = requests.post(f'%s/add' % ctx.obj.server,
+    response = requests.post(ctx.obj.server+'/add',
                              params={'word': ctx.obj.word})
-    click.echo(response.json()["output"])
+    output = response.json()['output']
+    click.echo(output)
 
 
 @cli.command()
 @click.pass_context
 def delete(ctx):
     """Delete word"""
-    response = requests.post(f'%s/delete' %
-                             ctx.obj.server, params={'word': ctx.obj.word})
-    click.echo(response.json()["output"])
+    response = requests.post(ctx.obj.server+'/delete',
+                             params={'word': ctx.obj.word})
+    output = response.json()['output']
+    click.echo(output)
 
 
 @cli.command()
 @click.pass_context
 def search(ctx):
     """Search word"""
-    response = requests.get(f'%s/search' %
-                            ctx.obj.server, params={'word': ctx.obj.word})
-    click.echo(response.json()["output"])
+    response = requests.get(ctx.obj.server+'/search',
+                            params={'word': ctx.obj.word})
+    output = response.json()['output']
+    click.echo(output)
 
 
 @cli.command()
 @click.pass_context
 def autocomplete(ctx):
     """Autocomplete suggestions"""
-    response = requests.get(f'%s/autocomplete' %
-                            ctx.obj.server, params={'word': ctx.obj.word})
-    click.echo(response.json()["output"])
+    response = requests.get(ctx.obj.server+'/autocomplete',
+                            params={'word': ctx.obj.word})
+    output = response.json()['output']
+    click.echo(output)
 
 
 @cli.command()
 @click.pass_context
 def display(ctx):
     """Display trie"""
-    response = requests.get(f'%s/display' % ctx.obj.server)
-    click.echo(response.json()["output"])
+    response = requests.get(ctx.obj.server+'/display')
+    output = response.json()['output']
+    click.echo(output)
 
 
 if __name__ == '__main__':
